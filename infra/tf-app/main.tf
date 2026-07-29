5ff0f93e111fd58d1a3b56afe221e442542a4c6e
RuaaThamer <156051638+RuaaThamer@users.noreply.github.com>
2026-07-29 19:00:35 -0400

    fix: update resource group name to app

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -5,15 +5,15 @@ resource "azurerm_resource_group" "app" {
 # Create a Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "app-vnet"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.app.location
+  resource_group_name = azurerm_resource_group.app.name
   address_space       = ["10.0.0.0/16"]
 }
 
 # Create a Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "app-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.app.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
